7d95b1348eb07149661a5e75a86d5360dc25077b
Alistair Sutherland <alistair@hodei.co.uk>
2024-03-07 19:51:03 +0000

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -xe
 
-echo "Testing"
-# Copy file from S3 bucket to required destination folder
-#aws s3 cp s3://hodei-nodedemodeploy/*.js /usr/local/webapp/
-#aws s3 cp s3://hodei-nodedemodeploy/*.json /usr/local/webapp/
+npm install --save
+node app.js
 
