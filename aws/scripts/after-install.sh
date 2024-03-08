0def5e4e131abdfbcc720f972f1aa16aca4f71e1
Alistair Sutherland <alistair@hodei.co.uk>
2024-03-08 10:06:13 +0000

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -xe
 
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-#source ~/.bashrc
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# source ~/.bashrc
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-#nvm install --lts
+# nvm install --lts
 
 npm install --save
