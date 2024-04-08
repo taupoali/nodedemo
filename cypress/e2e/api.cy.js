0792805cb8a5c091e56c39dad244cf9c29a6e657
taupoali <alistair@hodei.co.uk>
2024-04-08 14:56:19 +0100

    Add cypress testing

---
+++ cypress/e2e/api.cy.js
@@ -0,0 +1,41 @@
+
+describe('API Test Fetch of Counter', () => {
+    it('should successfully retrieve count data', () => {
+      cy.request('GET', '/')
+        .then((response) => {
+          expect(response.status).to.equal(200)
+          expect(response.body).to.have.property('count')
+        })
+    })
+
+    it('should increment the counter', () => {
+      cy.request('PUT', '/inc')
+        .then((response) => {
+          expect(response.status).to.equal(204)
+        })
+    })
+
+    it('test the counter increased', () => {
+      cy.request('GET', '/')
+        .then((response) => {
+          expect(response.status).to.equal(200)
+          expect(response.body.count).to.equal(1)
+        })
+    })
+
+    it('test the counter decreased', () => {
+      cy.request('PUT', '/dec')
+        .then((response) => {
+          expect(response.status).to.equal(204)
+        })
+    })
+
+    it('test the counter decreased', () => {
+      cy.request('GET', '/')
+        .then((response) => {
+          expect(response.status).to.equal(200)
+          expect(response.body.count).to.equal(0)
+        })
+    })
+
+  })
